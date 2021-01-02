a7900078f418c2f619e874fa47f19a61feca16c4
Vladlen Rodionov <72477919+vrodi18@users.noreply.github.com>
2021-01-02 14:20:42 -0600

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
     backend "s3" {
-        bucket = "jenkins-terraform-evolvecybertraining"
+        bucket = "terraform-iaac-2020-vladlen"
         key = "terraform_vpc/us-east-1/class/dev/infrastructure.tfstate"
         region = "us-east-1"
     }
